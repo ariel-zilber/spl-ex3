d61da55359e09545a93e18ee1ead3ea9000e4231
ariel-zilber <workarikzil@gmail.com>
2023-01-06 15:37:55 +0200

    working on server

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImp.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImp.java
@@ -24,38 +24,6 @@ public class StompMessagingProtocolImp  implements  StompMessagingProtocol<Strin
             Frame frame=new FrameFactory().getFrame(message);
             frame.process(connectionId,connections,this);
         }
-
-
-        if(message.length() > 0){
-            if(message.charAt(0) == '\n')
-                message = message.substring(1);
-            String[] msg = message.split("\\r?\\n"); // split the message by end line
-            switch (msg[0]) {
-                case "CONNECT":
-                    ConnectFrame connect = new ConnectFrame();
-                    connect.process(msg, connectionId, connections, this);
-                    break;
-                case "SUBSCRIBE":
-                    SubscribeFrame subscribe = new SubscribeFrame();
-                    subscribe.process(msg, connectionId, connections, this);
-                    break;
-                case "UNSUBSCRIBE":
-                    UnsubscribeFrame unsubscribeFrame = new UnsubscribeFrame();
-                    unsubscribeFrame.process(msg, connectionId, connections, this);
-                    break;
-                case "SEND":
-                    SendFrame send = new SendFrame();
-                    send.process(msg, connectionId, connections, this);
-                    break;
-                case "DISCONNECT":
-                    DisconnectFrame disconnectFrame = new DisconnectFrame();
-                    disconnectFrame.process(msg, connectionId, connections, this);
-                    break;
-                default:
-                    System.out.println("Incorrect Message received");
-                    break;
-            }
-        }
     }
 
 
