6ba502a166dca997d0deb30a33fd7abac1ebf446
ariel-zilber <workarikzil@gmail.com>
2023-01-06 14:20:43 +0200

    started working on client

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -1,4 +1,24 @@
+#include <stdlib.h>
+#include "../include/ConnectionHandler.h"
+
+// login {host:port} {username} {password}
+// join {game_name}
+// exit {game_name}
+// report {file}
+// summary {game_name} {user} {file}
+// logout
+
 int main(int argc, char *argv[]) {
+
+
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
+        return -1;
+    }
+
+
+
+
 	// TODO: implement the STOMP client
 	return 0;
 }
